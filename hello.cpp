3fa7ac62695d515702b24b202421ba5c5d7a2ddc
Gilboot <ssenyonjogilbert@gmail.com>
2021-02-07 14:09:46 +0300

    used enums

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,18 @@
 #include <cstdlib>
 #include <iostream>
 
+using namespace std;
+
 /* This program inputs two numbers x and y and outputs their sum */
 int main() {
-    int x, y;
-    std::cout << "Please enter two numbers: ";
-    std::cin >> x >> y;
-    int sum = x + y;
-    std::cout << "Their sum is " << sum << std::endl;
+
+    enum Day { SUM, MON, TUE, WED };
+    enum Mood { HAPPY = 3, SAD = 1, ANXIOUS = 4, SLEEPY = 2};
+
+    Day today = MON;
+    Mood currentMood = HAPPY;
+
+    cout << today << endl;
+    cout << currentMood << endl;
     return EXIT_SUCCESS;
 }
